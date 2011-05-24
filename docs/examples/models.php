172340db02f5d856b16c083a67182577de2b060f
Tris Forster <tris.git@tfconsulting.com.au>
2011-05-24 13:48:33 +1000

    Updated examples to use new meta definition style

--- docs/examples/models.php
+++ docs/examples/models.php
@@ -6,39 +6,33 @@
 * @filesource
 */
 class Blog extends Dormio_Model {
-  static function getMeta() {
-    return array(
-      'fields' => array(
-        'title' => array('type' => 'string', 'max_length' => 30),
-        'body' => array('type' => 'text'),
-        'author' => array('type' => 'foreignkey', 'model' => 'User'),
-        'comments' => array('type' => 'reverse', 'model' => 'Comment'),
-      ),
-    );
-  }
+  static $meta = array(
+    'fields' => array(
+      'title' => array('type' => 'string', 'max_length' => 30),
+      'body' => array('type' => 'text'),
+      'author' => array('type' => 'foreignkey', 'model' => 'User'),
+      'comments' => array('type' => 'reverse', 'model' => 'Comment'),
+    ),
+  );
 }
 
 class Comment extends Dormio_Model {
-  static function getMeta() {
-    return array(
-      'fields' => array(
-        'blog' => array('type' => 'foreignkey', 'model' => 'Blog'),
-        'body' => array('type' => 'text'),
-        'author' => array('type' => 'foreignkey', 'model' => 'User'),
-      ),
-    );
-  }
+  static $meta = array(
+    'fields' => array(
+      'blog' => array('type' => 'foreignkey', 'model' => 'Blog'),
+      'body' => array('type' => 'text'),
+      'author' => array('type' => 'foreignkey', 'model' => 'User'),
+    ),
+  );
 }
 
 class User extends Dormio_Model {
-  static function getMeta() {
-    return array(
-      'fields' => array(
-        'username' => array('type' => 'string', 'max_length' => 50),
-        'password' => array('type' => 'password'),
-      ),
-    );
-  }
+  static $meta = array(
+    'fields' => array(
+      'username' => array('type' => 'string', 'max_length' => 50),
+      'password' => array('type' => 'password'),
+    ),
+  );
   
   /**
   * This overrides the default display for the object.
@@ -50,14 +44,12 @@ class User extends Dormio_Model {
 }
 
 class Profile extends Dormio_Model {
-  static function getMeta() {
-    return array(
-      'fields' => array(
-        'user' => array('type' => 'onetoone', 'model' => 'User'),
-        'fav_colour' => array('type' => 'string', 'max_length' => 10),
-        'age' => array('type' => 'integer'),
-      ),
-    );
-  }
+  static $meta = array(
+    'fields' => array(
+      'user' => array('type' => 'onetoone', 'model' => 'User'),
+      'fav_colour' => array('type' => 'string', 'max_length' => 10),
+      'age' => array('type' => 'integer'),
+    ),
+  );
 }
 ?>
