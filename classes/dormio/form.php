d93ef3deacac440bf406b0307a51a2abdcd595ea
Tris Forster <tris.git@tfconsulting.com.au>
2011-12-09 10:38:35 +1100

    Updated Form for new Model get() accessors

--- classes/dormio/form.php
+++ classes/dormio/form.php
@@ -40,7 +40,7 @@ class Dormio_Form extends Phorms_Forms_Form{
     foreach($this->obj->_meta->fields as $name => $spec) {
       if(isset($spec['is_field'])) {
         $this->model_fields[$name] = $spec;
-        if($this->obj->ident()) $data[$name] = $this->obj->_getData($spec['db_column']);
+        if($this->obj->ident()) $data[$name] = $this->obj->_getModelData($spec['db_column']);
       }
     }
     parent::__construct(Phorms_Forms_Form::POST, false, $data);
