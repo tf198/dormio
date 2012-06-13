442fdc4611111517b63e5ffb8e4fbcde4b4704e9
Tris Forster <tris.git@tfconsulting.com.au>
2012-06-13 21:12:36 +1000

    Introspection tests

---
+++ tests/IntrospectionTest.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'DBTest.php';
+
+class Dormio_IntrospectionTest extends Dormio_DBTest{
+	function testTableNames() {
+		$this->load('data/entities.sql');
+		$dialect = Dormio_Dialect::factory('sqlite');
+
+		$stmt = $this->pdo->prepare($dialect->tableNames());
+		$stmt->execute();
+
+		$expected = array('blog', 'blog_tag', 'comment', 'comment_x_tag', 'multidep', 'multidep_x_multidep', 'profile', 'tag', 'tree', 'user');
+		$this->assertEquals($expected, $stmt->fetchAll(PDO::FETCH_COLUMN, 0));
+	}
+}
