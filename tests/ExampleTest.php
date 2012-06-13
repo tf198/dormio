7a036be20e76a1623558596dc731e34678a7ef00
Tris Forster <tris.git@tfconsulting.com.au>
2012-06-13 17:22:30 +1000

    Added missing Schema class and added benchmark to test suite

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -8,9 +8,20 @@ class ExampleTest extends PHPUnit_Framework_TestCase {
 		$this->assertOutput('schema');
 	}
 	
+	/**
+	 * Check we haven't introduced any memory leaks
+	 */
+	function testBenchmark() {
+		$last = exec('php benchmark.php', $output, $ret);
+		$this->assertEquals(0, $ret, "Failed to run benchmark script");
+		$scores = sscanf(array_pop($output), "%f %f | %f %f");
+		$this->assertLessThan(170, $scores[0], "Execution time");
+		$this->assertLessThan(1200, $scores[2], "Memory usage");
+	}
+	
 	function assertOutput($example) {
 		exec("php docs/examples/{$example}.php", $output, $ret);
-		$this->assertEquals(0, $ret);
+		$this->assertEquals(0, $ret, "Failed to run {$example}.php");
 		$expected = file("docs/examples/{$example}.out", FILE_IGNORE_NEW_LINES);
 		$this->assertTrue($expected == $output, "Output from '{$example}.php' differs from expected");
 	}
